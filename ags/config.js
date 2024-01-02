559726982e75acf20df62f6fbad5b99d7a1426b8
vnuxa <123472826+vnuxa@users.noreply.github.com>
2024-01-02 17:21:42 +0200

    Add files via upload

--- ags/config.js
+++ ags/config.js
@@ -7,6 +7,8 @@ import Bar from './widgets/bar/main.js';
 import Applauncher from './widgets/app_launcher/main.js';
 import notification_center from './widgets/notification_center/main.js';
 import load_notification from './widgets/notification_center/load_notification.js';
+import calendar_panel from './widgets/panels/calendar/main.js';
+// import battery_panel from './widgets/panels/battery/main.js'
 // widgets can be only assigned as a child in one container
 // so to make a reuseable widget, just make it a function
 // then you can use it by calling simply calling it
@@ -20,6 +22,9 @@ export default {
         Bar(),
         Applauncher(),
         notification_center(),
+        calendar_panel(),
+        // ModuleCalendar(),
+        // battery_panel(),
         // load_notification(),
         // you can call it, for each monitor
         // Bar({ monitor: 0 }),
