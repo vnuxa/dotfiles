91f7f522f23106bd071889598a004ef1155e296c
vnuxa <123472826+vnuxa@users.noreply.github.com>
2024-02-15 12:44:43 +0200

    Add files via upload

---
+++ nixos/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Nixos config flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    ags.url = "github:Aylur/ags";
+    hyprland.url = "github:hyprwm/Hyprland";
+
+    # home-manager = {
+    #   url = "github:nix-community/home-manager";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      nixosConfigurations.default = nixpkgs.lib.nixosSystem { # to add more hosts/users just change the "default" word since that litteraly IS a host name
+          specialArgs = {inherit inputs;};
+          modules = [ 
+            ./configuration.nix
+            # inputs.home-manager.nixosModules.default
+          ];
+        };
+
+    };
+}
