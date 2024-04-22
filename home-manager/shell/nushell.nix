71958a5a9c7114be5e5493b50598d05068cf9e0e
vnuxa <123472826+vnuxa@users.noreply.github.com>
2024-04-22 21:43:00 +0300

    Add files via upload

---
+++ home-manager/shell/nushell.nix
@@ -0,0 +1,69 @@
+{ config, pkgs, lib,...}:
+{
+    home.packages = [
+        pkgs.nushell # i enable in the main nixos-config
+
+        pkgs.carapace # for completion and other cool stuff
+        pkgs.starship # for nice looking prompts
+    ];
+
+    programs = {
+        nushell = {
+            enable = true;
+            configFile.source = ./nu/config.nu;
+            #shellAliases = {  }
+            extraConfig = ''
+
+                let carapace_completer = {|spans|
+                    carapace $spans.0 nushell $spans | from json
+                }
+            $env.config = {
+             show_banner: false,
+             completions: {
+             case_sensitive: false # case-sensitive completions
+                    quick: true    # set to false to prevent auto-selecting completions
+                    partial: true    # set to false to prevent partial filling of the prompt
+                    algorithm: "fuzzy"    # prefix or fuzzy
+                    external: {
+                        # set to false to prevent nushell looking into $env.PATH to find more suggestions
+                        enable: true 
+                        # set to lower can improve completion performance at the cost of omitting some options
+                        max_results: 100 
+                        completer: $carapace_completer # check 'carapace_completer' 
+                    }
+             }
+            } 
+            $env.PATH = ($env.PATH | 
+                    split row (char esep) |
+                    prepend /home/myuser/.apps |
+                    append /usr/bin/env
+                    )
+                '';
+        };
+
+        carapace = {
+            enable = true;
+            enableNushellIntegration = true;
+        };
+
+        starship = {
+            # Pipes ╰─ ╭─
+            # Powerline symbols                                     
+            # Wedges 🭧🭒 🭣🭧🭓
+            # Random noise 🬖🬥🬔🬗
+            # ┌ ─ │└ 
+            enable = true;
+            settings = {
+                add_newline = false;
+                character = {
+                    #TODO: find out what style i like
+                    success_symbol = "[➜](bold green)";
+                    error_symbol = "[➜](bold red)";
+                };
+            };
+            # fomat = ''\
+            # $'';
+        };
+
+    };
+}
